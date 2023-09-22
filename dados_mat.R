0e6ce81d71bca466ef8b2133dc116b1589f0efa9
Mateus <mateuszaparoli7@gmail.com>
2023-09-22 14:03:46 -0300

    add title

--- dados_mat.R
+++ dados_mat.R
@@ -12,7 +12,7 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-setwd("C:\\Users\\User\\Desktop\\Mateus\\ufmg\\Iniciacao_cientifica\\enem\\info_enem\\DADOS\\R_analise")  
+setwd("C:\\Users\\User\\Desktop\\Mateus\\ufmg\\Iniciacao_cientifica\\enem\\info_enem\\DADOS")  
 
 #---------------
 # Aloca��o de mem�ria
@@ -24,25 +24,25 @@ memory.limit(24576)
 
 #itens_2022 <- data.table::fread(input='itens_prova_2022.csv',integer64='character')
 
-data <- data.table::fread(input= 'microdados_enem_2022.csv',integer64='character', nrows = 500000)
+data <- data.table::fread(input= 'microdados_enem_2022.csv',integer64='character', nrows = 1000000)
 
 dados_matematica <- data.frame(data$CO_PROVA_MT, data$TX_RESPOSTAS_MT)
 
 #dados_matematica$TX_RESPOSTAS_MT <- paste(" ", dados_matematica$TX_RESPOSTAS_MT)
 
 # Use a função separate para dividir a coluna em várias colunas, pulando a primeira
-dados_matematica <- separate(dados_matematica, data.TX_RESPOSTAS_MT, into = paste0("Letra_", 0:45), sep = "")
+dados_matematica <- separate(dados_matematica, data.TX_RESPOSTAS_MT, into = paste0("Question_", 0:45), sep = "")
 
 # Remova a coluna original "TX_RESPOSTAS_MT" que não é mais necessária
 dados_matematica <- dados_matematica[, -2]
 
-
+#dados_matematica <- separate(dados_matematica, data.TX_GABARITO_MT, into = paste0("Gabarito_", 0:45), sep = "")
 
 # Substitua "seu_data_frame" pelo nome do seu data frame
 resultado <- dados_matematica %>%
   filter(data.CO_PROVA_MT == 1078) %>%
-  select(starts_with("Letra_")) %>%
-  pivot_longer(cols = starts_with("Letra_"), names_to = "Coluna") %>%
+  select(starts_with("Question_")) %>%
+  pivot_longer(cols = starts_with("Question_"), names_to = "Coluna") %>%
   group_by(Coluna, value) %>%
   count() %>%
   arrange(Coluna, desc(n))
@@ -50,7 +50,11 @@ resultado <- dados_matematica %>%
 ggplot(resultado) +
   aes(x = value, y = n) +
   geom_col(fill = "#112446") +
+  labs(
+    x = "Questions",
+    y = "Amount",
+    title = "Distribution of Students' Answers in the 2022 ENEM",
+    subtitle = "Math Section 1M Students"
+  ) +
   theme_minimal() +
   facet_wrap(vars(Coluna))
-
-
